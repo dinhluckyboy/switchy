a6286c5e7ed886a4cc2b802a308ce7ef99d1db63
dinhce <dinhluckyboy@gmail.com>
2026-03-19 15:01:36 +0700

    done 21.56

--- src/switchy.js
+++ src/switchy.js
@@ -22,8 +22,9 @@ function Switchy(selector, option) {
 
   this._opt = Object.assign(
     {
+      classNameActive: "tab--active",
       remember: true,
-      //   onChange
+      onChange: null,
     },
     option
   );
@@ -39,56 +40,62 @@ function Switchy(selector, option) {
 
 Switchy.prototype._init = function () {
   const valueOfTab = this._searchParams.get(
-    this._selector.replace(/[^a-zA-Z0-9]/g, "")
+    this._sanitizeQueryParams(this._selector)
   );
   const tabActive =
     this._opt.remember && valueOfTab
       ? this._tabs.find(
           (tab) =>
-            tab.getAttribute("href").replace(/[^a-zA-Z0-9]/g, "") === valueOfTab
+            this._sanitizeQueryParams(tab.getAttribute("href")) === valueOfTab
         ) || this._tabs[0]
       : this._tabs[0];
 
-  this._activateTab(tabActive);
+  this._currentTab = tabActive;
+
+  this._activateTab(tabActive, false);
 };
 
 Switchy.prototype._handleTabClick = function () {
   this._tabs.forEach((tab) => {
     tab.onclick = (e) => {
       e.preventDefault();
-      this._activateTab(tab);
+      this._checkActiveTab(tab);
     };
   }); // handle tab click
 };
 
-Switchy.prototype._activateTab = function (elementTab) {
+Switchy.prototype._activateTab = function (elementTab, triggerOnchange = true) {
   this._tabs.forEach((tab) => {
-    if (tab.closest("li").classList.contains("tab--active")) {
-      tab.closest("li").classList.remove("tab--active");
+    if (tab.closest("li").classList.contains(this._opt.classNameActive)) {
+      tab.closest("li").classList.remove(this._opt.classNameActive);
     }
   }); // remove all class tab--active
-  elementTab.closest("li").classList.add("tab--active"); // add class active
 
+  elementTab.closest("li").classList.add(this._opt.classNameActive); // add class active
   this._panels.forEach((panel) => {
     panel.hidden = true;
   }); // hidden all panels
+
   const panelActive = document.querySelector(elementTab.getAttribute("href"));
+
   panelActive.hidden = false; // show panel
 
   // save tab reload
   if (this._opt.remember) {
     this._searchParams = new URLSearchParams(location.search);
     this._searchParams.set(
-      this._selector.replace(/[^a-zA-Z0-9]/g, ""),
-      elementTab.getAttribute("href").replace(/[^a-zA-Z0-9]/g, "")
+      this._sanitizeQueryParams(this._selector),
+      this._sanitizeQueryParams(elementTab.getAttribute("href"))
     );
-    console.log(decodeURIComponent(this._searchParams.toString()));
-    history.replaceState(null, "", `?${this._searchParams.toString()}`);
+    history.replaceState(null, "", `?${this._searchParams}`);
   }
 
   // onChange
-  if (typeof this._opt.onChange === "function") {
-    this._opt.onChange();
+  if (typeof this._opt.onChange === "function" && triggerOnchange) {
+    this._opt.onChange({
+      tabActive: elementTab,
+      panelActive,
+    });
   }
 };
 
@@ -96,13 +103,13 @@ Switchy.prototype.switch = function (input) {
   // input is  elementTab or selectorPanel
   let tabToActive = null;
   if (typeof input === "string") {
-    tabToActive = this._tabs.find((tab) => tab.getAttribute("href") === input);
+    tabToActive = this._tabs.find((tab) => tab.getAttribute("href") === input); // selector panel
     if (!tabToActive) {
       console.error(`${input} does not exits`);
       return;
     }
   } else if (this._tabs.includes(input)) {
-    tabToActive = input;
+    tabToActive = input; // element tab
   }
 
   if (!tabToActive) {
@@ -110,7 +117,7 @@ Switchy.prototype.switch = function (input) {
     return;
   }
 
-  this._activateTab(tabToActive);
+  this._checkActiveTab(tabToActive);
 };
 
 Switchy.prototype.destroy = function () {
@@ -119,4 +126,19 @@ Switchy.prototype.destroy = function () {
   this._container = null;
   this._tabs = null;
   this._panels = null;
+  this._currentTab = null;
+  this._opt = null;
+  this._searchParams = null;
+  this._selector = null;
 };
+
+Switchy.prototype._checkActiveTab = function (tab) {
+  if (this._currentTab !== tab) {
+    this._activateTab(tab);
+    this._currentTab = tab; // reset current tab
+  }
+};
+
+Switchy.prototype._sanitizeQueryParams = function (string) {
+  return string.replace(/[^a-zA-Z0-9]/g, "");
+}; // sanitize query parameters url
